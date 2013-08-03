a645cac0a12ee2e1bc9311f51fda95aa2980f80f
LustoMagusto <otakymen@mail.ru>
2013-08-03 11:53:33 -0700

    Update FlxObject.hx

--- flixel/FlxObject.hx
+++ flixel/FlxObject.hx
@@ -60,11 +60,11 @@ class FlxObject extends FlxBasic
 	 */
 	static public inline var WALL:Int	= LEFT | RIGHT;
 	/**
-	 * Special-case constant meaning any direction, used mainly by <code>allowCollisions</code> and <code>touching</code>.
+	 * Special-case constant meaning up, down and left, used mainly by <code>allowCollisions</code> and <code>touching</code>.
 	 */
 	static public inline var PLATFORM:Int	= LEFT | RIGHT | UP;
 	/**
-	 * Special-case constant meaning only the left and right sides, used mainly by <code>allowCollisions</code> and <code>touching</code>.
+	 * Special-case constant meaning any direction, used mainly by <code>allowCollisions</code> and <code>touching</code>.
 	 */
 	static public inline var ANY:Int	= LEFT | RIGHT | UP | DOWN;
 	
